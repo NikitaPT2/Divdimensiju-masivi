3e5a4bdfdb043f8152905538c857a5ad2dc0d3ae
Nikita <nikitogka@gmail.com>
2021-06-17 20:30:19 +0300

    Papildināju projektu ar kostrukciju, kura parslēgs jautājumus, papildināju to ar 1 jautājumu, lai parbaudīt

--- src/Test.java
+++ src/Test.java
@@ -8,13 +8,35 @@ public class Test {
 		String correct = "You are correct";
 	    String incorrect = "You are incorrect";
 	    String playerAnswerString = " ";
-	    String[] quizQuestion = {"What is the color of the sky?", "What is the color of the sea?", "What is the color of the earth?"};
+	    String[] quizQuestion = {"Valoda Java ir viena no ... valodām"};
 	    playerAnswerString.toUpperCase();
 	    char playerAnswer = playerAnswerString.charAt(0);
 	    JOptionPane.showMessageDialog(null, "Pārbaudi savas zināšanas!");
 	    JOptionPane.showMessageDialog(null, "Izvēlieties 2 vai 3 atbildes uz jautājumu (rakstiet atbildes bez atstarpes, piemēram, ABC)");
 	    
-	    
+	    for(x = 0; x < 10; x++)
+	    {
+	    	JOptionPane.showMessageDialog(null, quizQuestion[x]);
+	    	switch(x) {
+	    	case 0:
+	    	playerAnswerString = JOptionPane.showInputDialog(null, "A = Blue, B = Green, C = Brown");
+
+            System.out.println(playerAnswerString+"   "+playerAnswer);
+
+            if(playerAnswerString.equals("A"))
+            {
+                JOptionPane.showMessageDialog(null, correct);
+                score++;
+            }
+            else
+            {
+                JOptionPane.showMessageDialog(null, incorrect);
+            }
+            break;
+	    	}
+	   
+	        }
+	    }
 	  
 	}
 	
